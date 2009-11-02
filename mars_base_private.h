78c00ecdb5a3a819b4284054f77bbaa2ea693fd2
nake90 <nake90@terra.es>
2009-11-02 17:31:22 +0000

    Añadida información de Copyright (GNU-GPL).

--- mars_base_private.h
+++ mars_base_private.h
@@ -5,19 +5,19 @@
 #define MARS_BASE_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.1.1.1093"
+#define VER_STRING	"0.1.1.1094"
 #define VER_MAJOR	0
 #define VER_MINOR	1
 #define VER_RELEASE	1
-#define VER_BUILD	1093
+#define VER_BUILD	1094
 #define COMPANY_NAME	""
 #define FILE_VERSION	""
-#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define FILE_DESCRIPTION	"Design, build and maintain your own Mars-base"
 #define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	""
+#define LEGAL_COPYRIGHT	"GNU-GPL (c) by Alfonso Arbona Gimeno (All rights reserved)"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	""
+#define PRODUCT_NAME	"mars-base"
 #define PRODUCT_VERSION	""
 
 #endif /*MARS_BASE_PRIVATE_H*/
